a4d395beb27c5e88d847e10fcbb5d46c30cff672
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-09 22:04:40 +0200

    Remove static export to fix Sanity Studio build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove the output: 'export' line - this allows dynamic routes
   images: {
     unoptimized: true
   }
