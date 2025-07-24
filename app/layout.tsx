fb8bf4ccebfc97a92b74dc3fc1b778ffaacf9ae8
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-24 21:30:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,13 +135,11 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Favicons - Complete Set */}
+       {/* Favicons - Updated to match actual files */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#16a34a" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="apple-mobile-web-app-title" content="Invest in Puglia" />
         <meta name="application-name" content="Invest in Puglia" />
