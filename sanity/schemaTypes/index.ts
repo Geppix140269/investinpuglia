00cf9ab701f658429c30a07651b84abd8a025c6a
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-25 17:09:20 +0200

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,17 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from './blockContentType'
+import { categoryType } from './categoryType'
+import { postType } from './postType'
+import { authorType } from './authorType'
+import { blogPostType } from './blogPostType' // ✅ NEW
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    authorType,
+    blogPostType // ✅ NEW
+  ],
 }
